e0208b146fa50b5d2114082ea9d3bc41f1860066
Mateusz Skalski <skalskimatthew@gmail.com>
2015-05-01 22:04:24 +0200

    Loading collection of images + spinner + status bar _ PROTOTYPE

---
+++ PictureManager/PictureManager/ImageModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PictureManager
+{
+    public class ImageModel : INotifyPropertyChanged
+    {
+        private bool isProcessing;
+        private Image image;
+
+        public String ImagePath { get; set; }
+        public Image Image { 
+            get 
+            {
+                return image;
+            }
+            set
+            {
+                image = value;
+                RaisePropertyChanged("Image");
+            }
+        }
+        public bool IsProcessing
+        {
+            get
+            {
+                return isProcessing;
+            }
+            set 
+            {
+                isProcessing = value;
+                RaisePropertyChanged("IsProcessing"); 
+            }
+        }
+
+        public ImageModel(Image image, String imagePath, bool isProcessing)
+        {
+            this.image = image;
+            this.isProcessing = isProcessing;
+            this.ImagePath = imagePath;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- PictureManager/PictureManager/MainWindow.xaml.cs
+++ PictureManager/PictureManager/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +18,77 @@ using System.Windows.Shapes;
 
 namespace PictureManager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public String processingStatus;
+        public String ProcessingStatus
+        {
+            get
+            {
+                return processingStatus;
+            }
+            set
+            {
+                processingStatus = value;
+                RaisePropertyChanged("ProcessingStatus");
+            }
+        }
+
+        public ObservableCollection<ImageModel> lstImages { get; set; }
+
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+
+        private void btnLoadFolderPath_Click(object sender, RoutedEventArgs e)
+        {
+            lstImages.Clear();
+            List<string> lstFileNames = new List<string>(System.IO.Directory.EnumerateFiles(txtFolderPath.Text, "*.jpg"));
+            foreach (string fileName in lstFileNames)
+            {
+                lstImages.Add(new ImageModel(null, fileName, true));
+            }
+
+            worker.RunWorkerAsync();
+        }
+
+
+
         public MainWindow()
         {
+            lstImages = new ObservableCollection<ImageModel>();
+            this.DataContext = this;
+            worker.DoWork += worker_DoWork;
+            worker.WorkerReportsProgress = true;
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
             InitializeComponent();
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Image imgTemp = new Image();
+            imgTemp.Source = new BitmapImage(new Uri(lstImages.ElementAt(e.ProgressPercentage).ImagePath));
+            imgTemp.Height = imgTemp.Width = 100;
+            lstImages.ElementAt(e.ProgressPercentage).Image = imgTemp;
+            ProcessingStatus = e.ProgressPercentage + " z " + lstImages.Count + " wszystkich plików";
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            for(int i=0; i < lstImages.Count; i++)
+            {
+                lstImages.ElementAt(i).IsProcessing = false;
+                worker.ReportProgress(i);
+                Thread.Sleep(500);
+            }
         }
     }
 }
